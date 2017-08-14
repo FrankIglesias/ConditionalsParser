8b86926f9b2517e41122ac1957ca5cdc7394b688
FrankIglesias <frank.beatle@gmail.com>
2017-08-13 23:25:51 -0300

    Indenting code

--- test/parser.test.js
+++ test/parser.test.js
@@ -4,21 +4,21 @@ import { expect } from 'chai'
 import parse from '../src/parser'
 
 describe('parser', () => {
-    it('parses a true', () => {
-      expect(parse('true')).to.deep.equal(true)
-    })
-    it('parses a false', () => {
-      expect(parse('false')).to.deep.equal(false)
-    })
-    it('detect if then else expresions', () => {
-      expect(parse('if true then true else true')).to.deep.equal({
-   "condition": true,
-   "branch1": true,
-   "branch2": true
-});
-    })
+  it('parses a true', () => {
+    expect(parse('true')).to.deep.equal(true)
+  })
+  it('parses a false', () => {
+    expect(parse('false')).to.deep.equal(false)
+  })
+  it('detect if then else expresions', () => {
+    expect(parse('if true then true else true')).to.deep.equal({
+      "condition": true,
+      "branch1": true,
+      "branch2": true
+    });
+  })
 
-    it('fails when it should', () => {
-      expect(() => parse('abc')).to.throw()
-    })
+  it('fails when it should', () => {
+    expect(() => parse('abc')).to.throw()
+  })
 })
